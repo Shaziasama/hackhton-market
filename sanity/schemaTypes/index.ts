9de448732ba5b545b2c3de535c523403d4e22a6a
Shaziasama <shaziasamma921@gmail.com>
2025-02-07 16:25:12 +0500

    Add files via upload

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import products from './products'
+import orders from './orders'
+import contactForm from './contact'
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [products,orders,contactForm],
+}
